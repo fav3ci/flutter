23b8d37b42a401e2f35c2e7ef70dbf84daf5aacc
fav3ci <shufackm23@gmail.com>
2024-07-02 22:42:21 +0530

    Add files via upload

---
+++ myself.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "shufa";
+  int age = 19;
+  String course = "engineering";
+  String department = "computer science";
+  print("name= $name");
+  print("age= $age");
+  print("course= $course");
+  print("department= $department");
+}
